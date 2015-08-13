590348dfde9222b8b4a4a025b1aa4b0182f789ce
Jessica Frank <jessicafrank204@gmail.com>
2015-08-13 13:48:26 +0000

    stuff

--- models/message.rb
+++ models/message.rb
@@ -1,4 +1,3 @@
-
 apperance = ["You are a very beautiful human being", "I love the way you dress", ]
 personality = ["You are a very funny person", "You are a great person to be around", "Being with you brightens my day"]
 future = ["You have an amazing future ahead of you", "Everything will be up hill from here", "I'm astonished by all the success you will have in the future" ]
