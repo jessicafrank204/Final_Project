8d29434f45f6ef5584f49f297b27e501abb4a5fe
Jessica Frank <jessicafrank204@gmail.com>
2015-08-13 19:04:37 +0000

    jess

--- models/main.rb
+++ models/main.rb
@@ -1,11 +1,9 @@
-# require 'net/http' 
-# uri = URI('https://data.guidestar.org/v1/charitycheck/54-1774039')
+# require 'net/http'
+# uri = URI('https://data.guidestar.org/v1/advancedsearch?q=ein:54-1774039')
 
 # Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http| 
 #   request = Net::HTTP::Get.new uri.request_uri
-#   request.basic_auth 'a4a79fb77a5646c2bde17cd0dc9956b1'
-  
+#   request.basic_auth 'username' 'password'
 #   response = http.request request # Net::HTTPResponse object 
-
 #   puts response.body 
 # end 
---
+++ models/scraper.rb
@@ -0,0 +1,5 @@
+# require 'nokogiri'
+# require 'open-uri'
+# fidi_html = open('https://s3-us-west-2.amazonaws.com/nokogiri-scrape/index.html')
+# fidi_nokogiri = Nokogiri::HTML(fidi_html)
+# title = fidi_nokogiri.css(“h1”).text
