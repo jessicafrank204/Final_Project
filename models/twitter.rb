01c7d484cfd2afc900a3371fc9c6beea6250451d
Jessica Frank <jessicafrank204@gmail.com>
2015-08-15 16:39:43 +0000

    jessica did the twitter api

--- models/twitter.rb
+++ models/twitter.rb
@@ -10,18 +10,42 @@ class Search
 		  config.consumer_secret ='OEkmDdaG8f5BERjXyiDuFsZB8IagX215hbYG5yLyWnAj7ue4Pe'
 		  config.access_token =	'3422246944-Gx2ax4NnQ3bVAz0wGenXgDi026TK8lMnqRhfkVs'
 		  config.access_token_secret = 'cilAt91NZ0Ofo7Qji8ofcX0aYFCH4z33NOwLrJJGHaT5N'
-     @hashtag = hashtag 
+      @final_tweets=[]
+      @users=[]
     end
+     @hashtag = hashtag 
   end
   
   def request
-    my_uri = "https://api.twitter.com/1.1/search/tweets.json?q=%23#{@hashtag}" #converts the string to a URI - same thing basically as a url 
-    results = open(my_uri) #making a get request to the server AND returning the results as a JSON file
-    results2 = JASON.parse(results) #parsing the jason called results - splits the jason into a hash
-    puts "THIS ISSSSSSSSS #{results2}"
-#     results[:tweets]
+    #puts @client.search(@hashtag).to_h.inspect
+    @client.search(@hashtag).to_h[:statuses].each do |tweet| #there is a hash with the keys being statuses and search_metadata.  Statuses is an array of tweets; inside each tweet is a hash 
+      @final_tweets.push("#{tweet[:text]}<br>")      
+    end
+  
+  @client.search(@hashtag).to_h[:statuses].each do |tweet| 
+    @users.push("#{tweet[:user][:name]}<br><br>")      
+    end
+
+  @final = Hash[@final_tweets.zip @users] #combines the two arrays into one hash
+  
+    return @final
+#       puts key
+#    end 
+    #puts @client.search(@hashtag).to_h.inspect
+  
+    @client.search(@hashtag)    #already in the twitter api, the search puts the hashtage in a string and makes that a uri and then calls search results method which parses the json and returns a hash
+#     my_uri = "https://api.twitter.com/1.1/search/tweets.json?q=%23#{@hashtag}" #converts the string to a URI - same thing basically as a url - a uri is getting info from the server
+#     results = open(my_uri) #making a get request to the server AND returning the results as a JSON file
+#     results2 = JASON.parse(results) #parsing the jason called results - splits the jason into a hash
+#     puts "THIS ISSSSSSSSS #{results2}"
+# #     results[:tweets]
     
   end
+
+def return_message
+  @final.key
+end
+
   
     
 
